69af9e20d0262f2ea78762eca13a22e5a60b582f
Aram Koukia <aram.koukia@lgm.ca>
2018-09-06 17:10:16 -0700

    some ui fixes for renewal

--- src/components/renewal-page/renewal-page.tsx
+++ src/components/renewal-page/renewal-page.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, State, Prop } from '@stencil/core';
+import { Component, Element, State, Prop, Listen } from '@stencil/core';
 import { Coverage } from '../../global/interfaces';
 //import { fetchCoverages } from '../../global/http-service';
 
@@ -8,6 +8,18 @@ import { Coverage } from '../../global/interfaces';
 })
 export class RenewalPage {
 
+    @State() duration: number = 72;
+    durationMin : number = 12;
+    durationMax : number = 80;
+    durationSteps : number = 1;
+
+    @State() mileage : number = 80000;
+    mileageMin : number = 0;
+    mileageMax : number = 200000;
+    mileageSteps : number = 5000;
+
+    @State() price = Math.round((this.duration * this.mileage ) / 170000);
+
   //page: number = 1;
   //currentStyle: number = 2;
   @State() coverages: Array<Coverage>;
@@ -15,6 +27,19 @@ export class RenewalPage {
   @Prop({ connect: 'ion-toast-controller' }) toastCtrl: HTMLIonToastControllerElement;
 
 
+  @Listen('ionChange')
+  handleRatingValue(event) {
+    console.log('I got an event', event);
+    console.log(event.target.localName);
+    if (event.target.localName === 'ion-range' && event.target.name === "duration") {
+      this.duration = event.detail.value;
+    } else if (event.target.localName === 'ion-range' && event.target.name === "mileage") {
+      this.mileage = event.detail.value;
+    }
+
+    this.price = Math.round((this.duration * this.mileage ) / 170000);
+  }
+  
   @Element() el: Element;
 
   async componentDidLoad() {
@@ -39,47 +64,54 @@ export class RenewalPage {
        <ion-content>
                <ion-card>
         <ion-card-content>
-          <ion-card-title>
-            Mechanical Breakdown Protection Renew
+          <ion-card-title color="primary">
+            Mechanical Breakdown Protection
           </ion-card-title>
-            <p>
-              Coverages:
-            </p>
-            <ion-item>
-              <ion-input required type="text" placeholder="Renew for 5 years"></ion-input>
-            </ion-item>
-            <ion-item>
-              <ion-input required type="text" placeholder="Name on the card"></ion-input>
-            </ion-item>
-            <ion-item>
-              <ion-label>Amount Due (+ tax): 2,300$</ion-label>
-            </ion-item>
+
+                <ion-item align-items-end>
+                  <ion-label slot="start">Duration (Months)</ion-label> 
+                  <ion-label slot="end" color="primary" align-items-end>{this.duration}</ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-range min={this.durationMin} max={this.durationMax} step={this.durationSteps} snaps value={this.duration} color="primary" pin name="duration"></ion-range>
+                </ion-item>
+
+                  <ion-item>
+                     <ion-label slot="start">Mileage (km)</ion-label>
+                     <ion-label slot="end" color="primary">{this.mileage}</ion-label>
+                  </ion-item>
+                  <ion-item>
+                    <ion-range min={this.mileageMin} max={this.mileageMax} step={this.mileageSteps} snaps value={this.mileage} color="primary" pin name="mileage"></ion-range>
+                  </ion-item>
+                  
+            
+                  <ion-item>
+                     <ion-label slot="start">Monthly:</ion-label>
+                     <ion-label slot="end" color="primary"><b> ${this.price}</b></ion-label>
+                  </ion-item>
+
         </ion-card-content>
         </ion-card>
       <ion-card>
         <ion-card-content>
-          <ion-card-title>
+          <ion-card-title color="primary">
             Payment Information
           </ion-card-title>
-          <ion-item>
-            <img src="../assets/img/credit.png" class="creditcard"/>
-          </ion-item>
             <ion-item>
-              <ion-input required type="text" placeholder="Credit Card Number"></ion-input>
+              <ion-label position="floating">Credit Card Number</ion-label>
+              <ion-input required type="text" placeholder=""></ion-input>
             </ion-item>
             <ion-item>
-              <ion-input required type="text" placeholder="Name on the card"></ion-input>
+              <ion-label position="floating">Name on the card</ion-label>
+              <ion-input required type="text" placeholder=""></ion-input>              
             </ion-item>
             <ion-item>
-              <ion-label>Expiry</ion-label>
+              <ion-label position="floating">Expiry</ion-label>
               <ion-datetime display-format="MM/DD/YYYY"></ion-datetime>
             </ion-item>
             <ion-item>
-              <ion-input required type="text" placeholder="CVV"></ion-input>
-            </ion-item>
-            <ion-item>
-              <ion-label>Save for future use</ion-label>
-              <ion-toggle checked></ion-toggle>
+              <ion-label position="floating">CVV</ion-label>
+              <ion-input required type="text" placeholder=""></ion-input>
             </ion-item>
             <ion-button color="primary">Pay</ion-button>
 
